4dde250a92271f3345e90c157dbd12e0e6db7b60
surreptakos <djgatsakos@gmail.com>
2015-07-15 23:06:52 -0500

    a

---
+++ App.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class App {
+	public static void main(String[] args) {
+		// to create main method, type main and press ctrl+space
+		// to auto-format code, ctrl+shift+f
+		// type syso and press ctrl+space
+		// ctrl+shift+o to auto-import and remove unnecessary imports
+
+		System.out.println("Hello");
+
+		// prints to system error stream
+		System.err.println("dah");
+
+		// Alt-shift-r to rename all instance of a variable OR right-click and
+		// go to Refactor > Rename
+		ArrayList aList;
+		aList = new ArrayList();
+
+		// To find all locations in which you used a variable, right-click
+		// References > Project
+		
+		// Press F3 on a variable or type to jump to its declaration
+		
+		Temp temp;
+		
+		// Ctrl-D to delete lines
+		
+		ArrayList<Temp> list = new ArrayList<Temp>();
+	}
+	
+	/*
+	 * 
+	 */
+	
+	private static void func() {
+		
+	}
+}
